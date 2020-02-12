86e4b9e546feb16d0959ccab02842e2bf6e7c218
Kelliann Lafferty <lafferty.kelliann@gmail.com>
2020-02-11 18:46:01 -0800

    resuing the backdrop

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -11,7 +11,7 @@ const modal = props => (
       style={{
         transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
 
-        opacity: props.show
+        opacity: props.show ? '1' : '0'
       }}
     >
       {props.children}
